4cefa2154cc925a56d01b53dfda4d74bfdc24324
Yusuf Badurohman <yusuf.badurohman@mst.co.id>
2024-10-20 08:54:38 +0700

    feat: helper for array_pull

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -14,3 +14,17 @@ if (! function_exists('array_get')) {
       return Illuminate\Support\Arr::get($array, $key, $default);
   }
 }
+
+if (! function_exists('array_pull')) {
+  /**
+   * Remove an item from an array using "dot" notation.
+   *
+   * @param  array  $array
+   * @param  string  $key
+   * @return mixed
+   */
+  function array_pull(&$array, $key)
+  {
+      return Illuminate\Support\Arr::pull($array, $key);
+  }
+}
